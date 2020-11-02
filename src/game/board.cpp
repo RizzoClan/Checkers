a94d53fcd364ea1a53c45d31aab6ed9f57b3af31
Nick Rizzo <nickmike55@gmail.com>
2020-11-02 15:58:32 -0500

    [feat] Basic hello world program

---
+++ src/game/board.cpp
@@ -0,0 +1,18 @@
+// Standard Includes
+#include <iostream>
+#include <string>
+#include <sstream>
+
+// 3rd Party Includes
+
+// Our Includes
+#include "board.h"
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+
+int main(int argc, char* argv[]) {
+    cout << "Hello World" << endl;
+}
