b77ff8cb2af2042ce1cdd3b6e7faed5cf894b70d
Nick Rizzo <nickmike55@gmail.com>
2020-11-04 21:28:29 -0500

    [fix] Putting pieces (of wrong color) in correct spots

--- src/engine/CheckersEngine.cpp
+++ src/engine/CheckersEngine.cpp
@@ -95,17 +95,24 @@ bool CheckersEngine::initPlayerOnBoard(const CheckersPlayer& player) {
     // determine offsets based on color 
     // (white first piece in row y=5)
     // (white first piece is in col x=1)
+    // white pieces cannot be placed passed middle
     const int y_offset = piece_type == BasicPieces::Red ? 0 : 5;
     const int x_offset = piece_type == BasicPieces::Red ? 0 : 1;
+    const int y_max    = piece_type == BasicPieces::Red ? getHeight() : 2; // inclusive for red 
 
-    // place in dark spots (x+y==odd)
-    for (int x = x_offset; x < getLength(); x++) {
-        // has to be odd and not in row 3 or 4
-        for (int y = y_offset; y < getHeight() && (x+y) % 2 == 1 && (y != 3 && y != 4); y++) {
-            // if fail to insert piece, failed and return
-            cout << "Placing piece @ " << BoardCoord{x,y} << endl;
-            if(!insertPiece(x, y, piece_type)) {
-                return false;
+    // go row by row (keep y constant)
+    for (int y = y_offset; y < getHeight() && y <= y_max; y++) {
+        for (int x = x_offset; x < getLength(); x++) {
+
+            // has to be odd and not in row 3 or 4
+            if ( ((x+y) % 2 == 1) && (y != 3 && y != 4) ) {
+
+                // if fail to insert piece, failed and return
+                // cout << "Placing piece @ " << BoardCoord{x,y} << endl;
+                if(!insertPiece(x, y, piece_type)) {
+                    return false;
+                }
+
             }
         }
     }
@@ -117,8 +124,9 @@ bool CheckersEngine::initPlayerOnBoard(const CheckersPlayer& player) {
 bool CheckersEngine::resetBoard() {
     for (CheckersPlayer& player_to_init : players) {
         if(!initPlayerOnBoard(player_to_init)) {
-            // if failed placing on board, reset failed & wipe board
-            cerr << "Failed inserting piece... resetting board to scratch" << endl;
+            // if failed placing on board, print it and reset/wipe board
+            cerr << "Failed inserting piece..." << endl << *this << endl;
+            cerr << "Resetting board to scratch" << endl;
             Board::resetBoard();
             return false;
         }
