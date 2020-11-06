71e08d27b18f577b57e527e61725039a24640983
Nick Rizzo <nickmike55@gmail.com>
2020-11-05 19:31:15 -0500

    [feat] Wrapped helper functions in namespace

--- src/include/helpers/common.hpp
+++ src/include/helpers/common.hpp
@@ -1,8 +1,13 @@
 #ifndef COMMON_HELPERS_HPP
 #define COMMON_HELPERS_HPP
 
+namespace Helpers {
+
+
 inline int GetSign(int _val) {
     return (_val > 0) - (_val < 0);
 }
+
+} // end of Helpers namespace
 
 #endif
